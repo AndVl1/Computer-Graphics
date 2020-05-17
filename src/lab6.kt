3a58d1fae81c5fdf00d49760f0dad84968d2a6ae
Andrey <and.vladislavov@gmail.com>
2020-05-17 15:11:43 +0300

    saving state lab6

--- src/lab6.kt
+++ src/lab6.kt
@@ -1,17 +1,18 @@
 package example
 
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
 import de.matthiasmann.twl.utils.PNGDecoder
 import org.lwjgl.glfw.GLFW
 import org.lwjgl.glfw.GLFWErrorCallback
 import org.lwjgl.glfw.GLFWKeyCallback
-import org.lwjgl.glfw.GLFWMouseButtonCallbackI
 import org.lwjgl.opengl.GL
-import org.lwjgl.opengl.GL11
 import org.lwjgl.opengl.GL11.*
 import org.lwjgl.opengl.GL30.glGenerateMipmap
-import org.lwjgl.stb.*
-import org.lwjgl.opengl.GL30.glLightModelfv
 import org.lwjgl.system.MemoryUtil
+import java.io.File
+import java.io.FileInputStream
+import java.io.FileWriter
 import java.nio.ByteBuffer
 import kotlin.math.*
 
@@ -24,37 +25,43 @@ import kotlin.math.*
  *
  * В2. использование текстуры для определения свойств поверхности
  * (модулирование коэффициента диффузного отражения);
+ *
+ * 1 - сохранить
+ * 2 - загрузить
  */
 
 class Lab6 {
     private var window: Long = 0
     private var keyCallback: GLFWKeyCallback? = null
     private var errorCallback: GLFWErrorCallback? = null
-    private var colorMain = floatArrayOf(.0f, .0f, .0f)
-    private var rotationX = .0f
-    private var rotationY = .0f
-    private var rotationZ = .0f
-    private var moveX = .0f
-    private var moveY = .0f
-    private var moveZ = .0f
-    private var sizeX = .6f
-    private var sizeY = .6f
-    private var sizeZ = .6f
-    private var mode = GL_LINE
-    private val small = .15f
-    private var faces = 30
-    private var step = (2 * PI / faces.toFloat()).toFloat()
-    private var height = 1f
+    private var state = State()
+    private val vpWidth = 1500
+    private val vpHeight = 1000
     private var top = ArrayList<ArrayList<Triple<Float, Float, Float>>>()
     private var bottom = ArrayList<ArrayList<Triple<Float, Float, Float>>>()
     private var sidesTop = ArrayList<Triple<Float, Float, Float>>()
     private var sidesBottom = ArrayList<Triple<Float, Float, Float>>()
-    private var linesCount = 50
+    private var step = (2 * PI / state.faces.toFloat()).toFloat()
     private var lines = ArrayList<ArrayList<Triple<Float, Float, Float>>>()
-    private val vpWidth = 1500
-    private val vpHeight = 1000
 
-    private lateinit var texture: example.texture.Texture
+    class State{
+        var colorMain = floatArrayOf(.0f, .0f, .0f)
+        var rotationX = .0f
+        var rotationY = .0f
+        var rotationZ = .0f
+        var moveX = .0f
+        var moveY = .0f
+        var moveZ = .0f
+        var sizeX = .6f
+        var sizeY = .6f
+        var sizeZ = .6f
+        var mode = GL_LINE
+        var faces = 30
+        var height = 1f
+        var linesCount = 50
+    }
+
+//    private lateinit var texture: example.texture.Texture
 
     private fun init() {
         GLFW.glfwSetErrorCallback(GLFWErrorCallback.createPrint(System.err).also { errorCallback = it })
@@ -73,48 +80,69 @@ class Lab6 {
                     )
                 } else if (action == GLFW.GLFW_PRESS || action == GLFW.GLFW_REPEAT) {
                     when (key) {
-                        GLFW.GLFW_KEY_UP -> rotationX += 5f
-                        GLFW.GLFW_KEY_DOWN -> rotationX -= 5f
-                        GLFW.GLFW_KEY_LEFT -> rotationY += 5f
-                        GLFW.GLFW_KEY_RIGHT -> rotationY -= 5f
+                        GLFW.GLFW_KEY_UP -> state.rotationX += 5f
+                        GLFW.GLFW_KEY_DOWN -> state.rotationX -= 5f
+                        GLFW.GLFW_KEY_LEFT -> state.rotationY += 5f
+                        GLFW.GLFW_KEY_RIGHT -> state.rotationY -= 5f
                         GLFW.GLFW_KEY_SPACE -> changeMode()
-                        GLFW.GLFW_KEY_W -> moveY += .15f
-                        GLFW.GLFW_KEY_S -> moveY -= .15f
-                        GLFW.GLFW_KEY_A -> moveX -= .15f
-                        GLFW.GLFW_KEY_D -> moveX += .15f
-                        GLFW.GLFW_KEY_O -> moveZ += .15f
-                        GLFW.GLFW_KEY_K -> moveZ -= .15f
+                        GLFW.GLFW_KEY_W -> state.moveY += .15f
+                        GLFW.GLFW_KEY_S -> state.moveY -= .15f
+                        GLFW.GLFW_KEY_A -> state.moveX -= .15f
+                        GLFW.GLFW_KEY_D -> state.moveX += .15f
+                        GLFW.GLFW_KEY_O -> state.moveZ += .15f
+                        GLFW.GLFW_KEY_K -> state.moveZ -= .15f
                         GLFW.GLFW_KEY_P -> {
-                            sizeX += .01f
-                            sizeY += .01f
-                            sizeZ += .01f
+                            state.sizeX += .01f
+                            state.sizeY += .01f
+                            state.sizeZ += .01f
                         }
                         GLFW.GLFW_KEY_M -> {
-                            sizeX -= .01f
-                            sizeY -= .01f
-                            sizeZ -= .01f
+                            state.sizeX -= .01f
+                            state.sizeY -= .01f
+                            state.sizeZ -= .01f
                         }
                         GLFW.GLFW_KEY_PAGE_UP -> {
-                            faces += 1
-                            step = (2 * PI / faces).toFloat()
+                            state.faces += 1
+                            step = (2 * PI / state.faces).toFloat()
                             calc()
                         }
                         GLFW.GLFW_KEY_PAGE_DOWN -> {
-                            if (faces > 3) {
-                                faces -= 1
-                                step = (2 * PI / faces).toFloat()
+                            if (state.faces > 3) {
+                                state.faces -= 1
+                                step = (2 * PI / state.faces).toFloat()
                                 calc()
                             }
                         }
                         GLFW.GLFW_KEY_HOME -> {
-                            if (linesCount > 2) {
-                                linesCount--
+                            if (state.linesCount > 2) {
+                                state.linesCount--
                                 calc()
                             }
                         }
                         GLFW.GLFW_KEY_END -> {
-                            linesCount++
+                            state.linesCount++
                             calc()
+                        }
+                        GLFW.GLFW_KEY_1 -> {
+                            val gson = GsonBuilder()
+                                .setPrettyPrinting()
+                                .create()
+                            val fileWriter = FileWriter("state.json")
+                            gson.toJson(state, fileWriter)
+                            fileWriter.flush()
+                            fileWriter.close()
+                        }
+                        GLFW.GLFW_KEY_2 -> {
+                            try {
+                                val stream = FileInputStream("state.json")
+                                val jsonBytes = stream.readAllBytes()
+                                val json = String(jsonBytes)
+                                state = Gson().fromJson(json, state::class.java)
+                                stream.close()
+                                calc()
+                            } catch (e: Exception) {
+                                e.printStackTrace()
+                            }
                         }
                     }
                 }
@@ -141,13 +169,13 @@ class Lab6 {
 //        drawCoords()
         drawPyramid()
         drawCube()
-        drawCubeTextured()
-//        light()
+//        drawCubeTextured()
+        light()
     }
 
     private fun calc() {
         var angle = 0f
-        val lineStep = height / linesCount
+        val lineStep = state.height / state.linesCount
 
         //SIDE
         sidesTop.clear()
@@ -155,8 +183,8 @@ class Lab6 {
         while (angle <= 2 * PI) {
             val x = cos(angle)
             val y = sin(angle)
-            sidesTop.add(Triple(x / 2, height, y / 2))
-            sidesBottom.add(Triple(x, 0f, y))
+        sidesTop.add(Triple(x / 2, state.height, y / 2))
+        sidesBottom.add(Triple(x, 0f, y))
             angle += step
         }
         sidesTop.add(sidesTop[0])
@@ -167,7 +195,7 @@ class Lab6 {
         lines.clear()
         var i: Float = lineStep
         var j = 0
-        while (i < height) {
+        while (i < state.height) {
             angle = 0f
             val level = ArrayList<Triple<Float, Float, Float>>()
             while (angle < 2 * PI) {
@@ -186,23 +214,23 @@ class Lab6 {
 
         //TOP
         top.clear()
-        var k = (.5f / linesCount)
+        var k = (.5f / state.linesCount)
         while (k <= .5f) {
             val tmp = ArrayList<Triple<Float, Float, Float>>()
-            tmp.add(Triple(0f, height, 0f))
+            tmp.add(Triple(0f, state.height, 0f))
             angle = 0f
             while (angle <= 2 * PI) {
-                tmp.add(Triple(k * cos(angle), height, k * sin(angle)))
+                tmp.add(Triple(k * cos(angle), state.height, k * sin(angle)))
                 angle += step
             }
             tmp.add(tmp[1])
             top.add(tmp)
-            k += (.5f / linesCount)
+            k += (.5f / state.linesCount)
         }
 
         //BOTTOM
         bottom.clear()
-        k = 1f / linesCount
+        k = 1f / state.linesCount
         while (k <= 1) {
             val tmp = ArrayList<Triple<Float, Float, Float>>()
             tmp.add(Triple(0f, 0f, 0f))
@@ -213,7 +241,7 @@ class Lab6 {
             }
             tmp.add(tmp[1])
             bottom.add(tmp)
-            k += 1f / linesCount
+            k += 1f / state.linesCount
         }
     }
 
@@ -242,11 +270,11 @@ class Lab6 {
     private fun drawPyramid() {
         glPushMatrix()
 
-        glTranslatef(moveX, moveY, moveZ)
-        glRotatef(rotationX, 1f, 0f, 0f)
-        glRotatef(rotationY, 0f, 1f, 0f)
-        glRotatef(rotationZ, 0f, 0f, 1f)
-        glScalef(sizeX, sizeY, sizeZ)
+        glTranslatef(state.moveX, state.moveY, state.moveZ)
+        glRotatef(state.rotationX, 1f, 0f, 0f)
+        glRotatef(state.rotationY, 0f, 1f, 0f)
+        glRotatef(state.rotationZ, 0f, 0f, 1f)
+        glScalef(state.sizeX, state.sizeY, state.sizeZ)
 
         // ***** //
 
@@ -261,7 +289,7 @@ class Lab6 {
 
         //LINES
 
-        if (mode != GL_FILL)
+        if (state.mode != GL_FILL)
             for (line in lines) {
                 glBegin(GL_LINE_STRIP)
                 for (l in line) {
@@ -327,120 +355,120 @@ class Lab6 {
         glBegin(GL_QUADS)
 
         glColor3f(1f, 0f, 0f)
-        glVertex3f(-small, -small, -small)
-        glVertex3f(-small, small, -small)
-        glVertex3f(small, small, -small)
-        glVertex3f(small, -small, -small)
+        glVertex3f(-SMALL, -SMALL, -SMALL)
+        glVertex3f(-SMALL, SMALL, -SMALL)
+        glVertex3f(SMALL, SMALL, -SMALL)
+        glVertex3f(SMALL, -SMALL, -SMALL)
 
         glColor3f(0f, 1f, 0f)
-        glVertex3f(-small, -small, small)
-        glVertex3f(-small, small, small)
-        glVertex3f(small, small, small)
-        glVertex3f(small, -small, small)
+        glVertex3f(-SMALL, -SMALL, SMALL)
+        glVertex3f(-SMALL, SMALL, SMALL)
+        glVertex3f(SMALL, SMALL, SMALL)
+        glVertex3f(SMALL, -SMALL, SMALL)
 
         glColor3f(0f, 0f, 1f)
-        glVertex3f(-small, small, -small)
-        glVertex3f(small, small, -small)
-        glVertex3f(small, small, small)
-        glVertex3f(-small, small, small)
+        glVertex3f(-SMALL, SMALL, -SMALL)
+        glVertex3f(SMALL, SMALL, -SMALL)
+        glVertex3f(SMALL, SMALL, SMALL)
+        glVertex3f(-SMALL, SMALL, SMALL)
 
         glColor3f(1f, 1f, 0f)
-        glVertex3f(-small, -small, -small)
-        glVertex3f(small, -small, -small)
-        glVertex3f(small, -small, small)
-        glVertex3f(-small, -small, small)
+        glVertex3f(-SMALL, -SMALL, -SMALL)
+        glVertex3f(SMALL, -SMALL, -SMALL)
+        glVertex3f(SMALL, -SMALL, SMALL)
+        glVertex3f(-SMALL, -SMALL, SMALL)
 
         glColor3f(1f, 1f, 1f)
-        glVertex3f(-small, -small, -small)
-        glVertex3f(-small, small, -small)
-        glVertex3f(-small, small, small)
-        glVertex3f(-small, -small, small)
+        glVertex3f(-SMALL, -SMALL, -SMALL)
+        glVertex3f(-SMALL, SMALL, -SMALL)
+        glVertex3f(-SMALL, SMALL, SMALL)
+        glVertex3f(-SMALL, -SMALL, SMALL)
 
         glColor3f(1f, 0f, .5f)
-        glVertex3f(small, -small, -small)
-        glVertex3f(small, small, -small)
-        glVertex3f(small, small, small)
-        glVertex3f(small, -small, small)
+        glVertex3f(SMALL, -SMALL, -SMALL)
+        glVertex3f(SMALL, SMALL, -SMALL)
+        glVertex3f(SMALL, SMALL, SMALL)
+        glVertex3f(SMALL, -SMALL, SMALL)
         glEnd()
 
         glPopMatrix()
     }
 
-    private fun drawCubeTextured() {
-
-        glPushMatrix()
-        glLoadIdentity()
-        glColor4f(1f, 1f, 1f, 1f)
-        glBindTexture(GL_TEXTURE_2D, texture.id)
-
-        glMatrixMode(GL_MODELVIEW)
-
-        glTranslatef(-.7f, -.7f, 0f)
-
-        glBegin(GL_QUADS)
-
-        glTexCoord2f(1f, 1f)
-        glVertex3f(-small, -small, -small)
-        glTexCoord2f(1f, 0f)
-        glVertex3f(-small, small, -small)
-        glTexCoord2f(0f, 0f)
-        glVertex3f(small, small, -small)
-        glTexCoord2f(0f, 1f)
-        glVertex3f(small, -small, -small)
-
-        glTexCoord2f(0f, 1f)
-        glVertex3f(-small, -small, small)
-        glTexCoord2f(0f, 0f)
-        glVertex3f(-small, small, small)
-        glTexCoord2f(1f, 0f)
-        glVertex3f(small, small, small)
-        glTexCoord2f(1f, 1f)
-        glVertex3f(small, -small, small)
-
-        glTexCoord2f(0f, 0f)
-        glVertex3f(-small, small, -small)
-        glTexCoord2f(1f, 0f)
-        glVertex3f(small, small, -small)
-        glTexCoord2f(1f, 1f)
-        glVertex3f(small, small, small)
-        glTexCoord2f(0f, 1f)
-        glVertex3f(-small, small, small)
-
-        glTexCoord2f(1f, 0f)
-        glVertex3f(-small, -small, -small)
-        glTexCoord2f(1f, 1f)
-        glVertex3f(small, -small, -small)
-        glTexCoord2f(0f, 1f)
-        glVertex3f(small, -small, small)
-        glTexCoord2f(0f, 0f)
-        glVertex3f(-small, -small, small)
-
-
-        glTexCoord2f(1f, 0f)
-        glVertex3f(-small, -small, -small)
-        glTexCoord2f(0f, 0f)
-        glVertex3f(-small, small, -small)
-        glTexCoord2f(0f, 1f)
-        glVertex3f(-small, small, small)
-        glTexCoord2f(1f, 1f)
-        glVertex3f(-small, -small, small)
-
-        glTexCoord2f(1f, 1f)
-        glVertex3f(small, -small, -small)
-        glTexCoord2f(1f, 0f)
-        glVertex3f(small, small, -small)
-        glTexCoord2f(0f, 0f)
-        glVertex3f(small, small, small)
-        glTexCoord2f(0f, 1f)
-        glVertex3f(small, -small, small)
-
-        glEnd()
-
-        glPopMatrix()
-
-        glBindTexture(GL_TEXTURE_2D, 0)
-        glDisable(GL_COLOR_MATERIAL)
-    }
+//    private fun drawCubeTextured() {
+//
+//        glPushMatrix()
+//        glLoadIdentity()
+//        glColor4f(1f, 1f, 1f, 1f)
+//        glBindTexture(GL_TEXTURE_2D, texture.id)
+//
+//        glMatrixMode(GL_MODELVIEW)
+//
+//        glTranslatef(-.7f, -.7f, 0f)
+//
+//        glBegin(GL_QUADS)
+//
+//        glTexCoord2f(1f, 1f)
+//        glVertex3f(-small, -small, -small)
+//        glTexCoord2f(1f, 0f)
+//        glVertex3f(-small, small, -small)
+//        glTexCoord2f(0f, 0f)
+//        glVertex3f(small, small, -small)
+//        glTexCoord2f(0f, 1f)
+//        glVertex3f(small, -small, -small)
+//
+//        glTexCoord2f(0f, 1f)
+//        glVertex3f(-small, -small, small)
+//        glTexCoord2f(0f, 0f)
+//        glVertex3f(-small, small, small)
+//        glTexCoord2f(1f, 0f)
+//        glVertex3f(small, small, small)
+//        glTexCoord2f(1f, 1f)
+//        glVertex3f(small, -small, small)
+//
+//        glTexCoord2f(0f, 0f)
+//        glVertex3f(-small, small, -small)
+//        glTexCoord2f(1f, 0f)
+//        glVertex3f(small, small, -small)
+//        glTexCoord2f(1f, 1f)
+//        glVertex3f(small, small, small)
+//        glTexCoord2f(0f, 1f)
+//        glVertex3f(-small, small, small)
+//
+//        glTexCoord2f(1f, 0f)
+//        glVertex3f(-small, -small, -small)
+//        glTexCoord2f(1f, 1f)
+//        glVertex3f(small, -small, -small)
+//        glTexCoord2f(0f, 1f)
+//        glVertex3f(small, -small, small)
+//        glTexCoord2f(0f, 0f)
+//        glVertex3f(-small, -small, small)
+//
+//
+//        glTexCoord2f(1f, 0f)
+//        glVertex3f(-small, -small, -small)
+//        glTexCoord2f(0f, 0f)
+//        glVertex3f(-small, small, -small)
+//        glTexCoord2f(0f, 1f)
+//        glVertex3f(-small, small, small)
+//        glTexCoord2f(1f, 1f)
+//        glVertex3f(-small, -small, small)
+//
+//        glTexCoord2f(1f, 1f)
+//        glVertex3f(small, -small, -small)
+//        glTexCoord2f(1f, 0f)
+//        glVertex3f(small, small, -small)
+//        glTexCoord2f(0f, 0f)
+//        glVertex3f(small, small, small)
+//        glTexCoord2f(0f, 1f)
+//        glVertex3f(small, -small, small)
+//
+//        glEnd()
+//
+//        glPopMatrix()
+//
+//        glBindTexture(GL_TEXTURE_2D, 0)
+//        glDisable(GL_COLOR_MATERIAL)
+//    }
 
     private fun rotateCoords() {
         glMatrixMode(GL_PROJECTION)
@@ -494,7 +522,7 @@ class Lab6 {
         glDepthFunc(GL_LEFT)
 
         try {
-            texture = example.texture.Texture.loadTexture("wood")
+//            texture = example.texture.Texture.loadTexture("wood")
 //            texture = Texture.loadTexture("wood")
         } catch (e: Exception) {
             e.printStackTrace()
@@ -509,10 +537,10 @@ class Lab6 {
 
             rotateCoords()
 
-            glClearColor(colorMain[0], colorMain[1], colorMain[2], 1.0f)
+            glClearColor(state.colorMain[0], state.colorMain[1], state.colorMain[2], 1.0f)
             glClear(GL_COLOR_BUFFER_BIT or GL_DEPTH_BUFFER_BIT)
 
-            glPolygonMode(GL_FRONT_AND_BACK, mode)
+            glPolygonMode(GL_FRONT_AND_BACK, state.mode)
 
             render()
 
@@ -535,12 +563,14 @@ class Lab6 {
     }
 
     private fun changeMode() {
-        mode = if (mode == GL_FILL) {
+        state.mode = if (state.mode == GL_FILL) {
             GL_LINE
         } else GL_FILL
     }
 
-
+    companion object {
+        const val SMALL = .15f
+    }
 }
 
 fun main() {
